524e434075cd77d1ffb7c844f197ccb29a78ba8c
Jazornowsky <damian.m.wojdat@gmail.com>
2013-05-25 06:36:09 -0700

    TalkTalk! Client 0.2 beta

---
+++ src/net/damian/wojdat/talktalk_client/MessageProcessor.java
@@ -0,0 +1,18 @@
+package net.damian.wojdat.talktalk_client;
+
+
+public class MessageProcessor {
+	
+	Connection connection = null;
+	WindowController windowController = null;
+	
+	public MessageProcessor(Connection connection, WindowController windowController) {
+		this.connection = connection;
+		this.windowController = windowController;
+	}
+	
+	
+	
+	
+	
+}
---
+++ src/net/damian/wojdat/talktalk_client/WindowController.java
@@ -0,0 +1,164 @@
+package net.damian.wojdat.talktalk_client;
+import static net.damian.wojdat.talktalk_server.TalkTalkCommands.CMD_MSG;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+
+public final class WindowController {
+
+	@FXML private static TextArea messagesArea;
+	@FXML private static TextArea logArea;
+    @FXML private static TextArea sendTextArea;
+    @FXML private static Button submitmessage;
+    @FXML private static TreeView<String> userListMain;
+    @FXML private static TreeItem<String> userListRoot = new TreeItem<>("Users");
+    
+	@FXML protected void handleSubmitButtonAction(ActionEvent event) {
+		if(Client.getInstance().connection.providedId == null)
+			putFormattedLog("[ERROR:] No ID provided by the server, you can't talk");
+		else {
+			if(sendTextArea.getText() == "" || sendTextArea.getText() == null)
+				return;
+			processClientMessage(sendTextArea.getText());
+			sendTextArea.setText("");
+		}
+	}
+	
+	@FXML protected void handleConnectButtonAction(ActionEvent event) {		
+		Client.getInstance().connection.connect();		
+	}
+	
+	@FXML protected void handleDisconnectButtonAction(ActionEvent event) {
+		Client.getInstance().connection.disconnect();		
+	}
+	
+	@FXML protected void resetSendTextArea(ActionEvent event) {
+		sendTextArea.clear();
+	}
+	
+	@FXML protected void onEnterHit(KeyEvent event) {
+		
+		if(event.isShiftDown()) {
+			return;
+		}
+		if(event.getCode() == KeyCode.ENTER) {
+			submitmessage.fire();
+			event.consume();
+		}
+	}
+	
+	public void updateUsersList(final HashMap<Integer,String> list) {
+		
+		System.out.println("[DEBUG:] updateUsersList");
+		Platform.runLater(new Runnable() {
+			@Override public void run() {
+				System.out.println(list.toString());
+				userListRoot.getChildren().clear();
+				userListRoot.setExpanded(true);
+				for(Integer key: list.keySet()) {
+					userListRoot.getChildren().add(new TreeItem<String>(list.get(key)));
+				}
+				userListMain.setRoot(userListRoot);
+			}
+		});
+		
+	}
+	
+	public TextArea getTextArea() {
+		return messagesArea;
+	}
+	
+	public TextArea getLogArea() {
+		return logArea;
+	}
+	
+	public TextArea getSendTextArea() {
+		return sendTextArea;
+	}
+	
+	public Button getSubmitButton() {
+		return submitmessage;
+	}
+	
+	public void putFormattedLog(String msg) {
+		
+		System.out.println("[DEBUG:] putFormattedLog");
+		
+		class PutFormattedLog implements Runnable {
+			String msg;
+			PutFormattedLog(String s) { msg = s; }
+			
+			public void run() {
+				
+				Calendar calendar = Calendar.getInstance();
+				calendar.getTime();
+				SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+				getLogArea().appendText("[" + sdf.format(calendar.getTime()) + "] " + msg + "\n");
+				
+			}
+		}
+		
+		Platform.runLater(new PutFormattedLog(msg));	
+		
+	}
+	
+	public void putFormattedMessage(String msg) {
+		
+		System.out.println("[DEBUG:] putFormattedMessage");
+		
+		class PutFormattedMessage implements Runnable {
+			String msg;
+			PutFormattedMessage(String s) { msg = s; }
+			
+			public void run() {
+				
+				Calendar calendar = Calendar.getInstance();
+				calendar.getTime();
+				SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+				getTextArea().appendText("[" + sdf.format(calendar.getTime()) + "] " + msg + "\n");
+				
+			}
+		}
+		
+		Platform.runLater(new PutFormattedMessage(msg));		
+		
+	}
+	
+	public void processClientMessage(String message) {
+		
+		Integer index = null;
+		
+		if(message != null && (index = message.indexOf(" ")) != -1) {
+			
+			switch(message.substring(0, index)) {
+				case CMD_MSG: {
+					putFormattedMessage("[" + Client.getInstance().getName() + ":] " + message.substring(index+1));
+					Client.getInstance().connection.writeObject(CMD_MSG + " [" + Client.getInstance().getName() + ":] " + message.substring(index+1));
+					return;
+				}
+				default: {
+					putFormattedMessage("[" + Client.getInstance().getName() + ":] " + message);
+					Client.getInstance().connection.writeObject(CMD_MSG + " [" + Client.getInstance().getName() + ":] " + message);
+					return;
+				}
+			}
+		}
+		else {
+			putFormattedMessage("[" + Client.getInstance().getName() + ":] " + message);
+			Client.getInstance().connection.writeObject(CMD_MSG + " [" + Client.getInstance().getName() + ":] " + message);
+		}
+	}
+	
+}
